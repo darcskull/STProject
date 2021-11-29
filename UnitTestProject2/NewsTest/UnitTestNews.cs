4f1d96a8927edee042d7b8437394e020a2549bce
darcskull <74406901+darcskull@users.noreply.github.com>
2021-11-29 16:20:54 +0200

    #41

--- UnitTestProject2/NewsTest/UnitTestNews.cs
+++ UnitTestProject2/NewsTest/UnitTestNews.cs
@@ -7,6 +7,8 @@ namespace UnitTestProgram
     [TestClass]
     public class UnitTestNews
     {
+        private News validNew = new News("name", "information");
+
         [TestMethod]
         public void News_Exception_InitialisationsNameNull()
         {
@@ -18,6 +20,26 @@ namespace UnitTestProgram
             Assert.ThrowsException<ArgumentException>(() => new News(" ", "Information"));//Името не може да бъде празен интервал
         }
        
+        [TestMethod]
+        public void News_Complete()
+        {
+            News news = new News(validNew.Name, validNew.Information);
+            Assert.AreEqual(news.Name,validNew.Name);
+            Assert.AreEqual(news.Information, validNew.Information);
+        }
 
+        [TestMethod]
+        public void News_DrawNews()
+        {
+            var textNews = validNew.DrawNews();
+            Assert.IsNotNull(textNews);
+        }
+
+        [TestMethod]
+        public void News_ReadNews()
+        {
+            var textNews = validNew.ReadNews();
+            Assert.IsNotNull(textNews);
+        }
     }
 }
