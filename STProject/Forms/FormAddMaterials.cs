454031fac8909404fe1933396e6b9931412f6086
Ivan Viyachki <il.viyachki@gmail.com>
2021-12-29 10:25:24 +0200

    Upload Materials Form

--- STProject/Forms/FormAddMaterials.cs
+++ STProject/Forms/FormAddMaterials.cs
@@ -1,4 +1,4 @@
-﻿using STProject.Classes;
+using STProject.Classes;
 using STProject.Core;
 using System;
 using System.Collections.Generic;
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace STProject
 {
@@ -17,6 +19,10 @@ namespace STProject
         DropDownValues vvalues = new DropDownValues();
         List<string> dropValyes = new List<string>();
         Teacher user = new Teacher();
+
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; AttachDbFilename=C:\Users\user\Downloads\STProject-master\STProject-master\STProject\DataBase\DBOnlineTrainingSystem.mdf; Integrated Security=True;");
+        SqlCommand cmd;
+
         public FormAddMaterials(Teacher u)
         {
             InitializeComponent();
@@ -37,13 +43,36 @@ namespace STProject
         {
 
         }
-
+       
         private void buttonBack_Click(object sender, EventArgs e)
         {
            FormMainPageTeacher formUser = new FormMainPageTeacher(user);
             this.Hide();
             formUser.ShowDialog();
             this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); 
+            if (result == DialogResult.OK) 
+            {
+                string file = openFileDialog1.FileName;
+
+                string[] f = file.Split('\\');
+               
+                string fn = f[(f.Length) - 1];
+                string dest = @"E:\New folder\" + fn;
+                string sb = comboBoxSubject.SelectedItem.ToString();
+               
+                File.Copy(file, dest, true);
+
+                string q1 = $"insert into Materials values(N'{fn}',N'{sb}',N'{dest}');";
+                cmd = new SqlCommand(q1, con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("success");
+            }
         }
     }
 }
